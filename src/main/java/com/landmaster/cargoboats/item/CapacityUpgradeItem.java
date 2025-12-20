7e3b0575beff2c473bae8b9799b72456406741e9
Landmaster <python0429@gmail.com>
2025-12-19 23:05:35 -0500

    add upgrade tooltips

--- src/main/java/com/landmaster/cargoboats/item/CapacityUpgradeItem.java
+++ src/main/java/com/landmaster/cargoboats/item/CapacityUpgradeItem.java
@@ -2,8 +2,16 @@ package com.landmaster.cargoboats.item;
 
 import com.landmaster.cargoboats.CargoBoats;
 import com.landmaster.cargoboats.Config;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+import javax.annotation.Nonnull;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CapacityUpgradeItem extends Item implements MotorboatUpgrade {
     public CapacityUpgradeItem(Properties properties) {
@@ -16,5 +24,25 @@ public class CapacityUpgradeItem extends Item implements MotorboatUpgrade {
             return Config.MOTORBOAT_FLUID_CAPACITY.get().size();
         }
         return Config.MOTORBOAT_ITEM_CAPACITY_MULTIPLIER.get().size();
+    }
+
+    @Override
+    public void appendHoverText(@Nonnull ItemStack stack, @Nonnull TooltipContext context, @Nonnull List<Component> tooltipComponents, @Nonnull TooltipFlag tooltipFlag) {
+        var itemMultipliers = Config.MOTORBOAT_ITEM_CAPACITY_MULTIPLIER.get();
+        var fluidCapacities = Config.MOTORBOAT_FLUID_CAPACITY.get();
+        for (int i=0; i<Math.max(itemMultipliers.size(), fluidCapacities.size()); ++i) {
+            var subComponents = new ArrayList<Component>(3);
+            subComponents.add(Component.literal(Integer.toString(i+1)));
+            if (i < itemMultipliers.size()) {
+                subComponents.add(Component.translatable("tooltip.cargoboats.capacity_upgrade.items", itemMultipliers.get(i)));
+            }
+            if (i < fluidCapacities.size()) {
+                subComponents.add(Component.translatable("tooltip.cargoboats.capacity_upgrade.fluids", fluidCapacities.get(i)));
+            }
+            tooltipComponents.add(Component.translatable(
+                    "tooltip.cargoboats.capacity_upgrade." + (subComponents.size()-1),
+                    subComponents.toArray()
+            ).withStyle(ChatFormatting.AQUA));
+        }
     }
 }
--- src/main/java/com/landmaster/cargoboats/item/SpeedUpgradeItem.java
+++ src/main/java/com/landmaster/cargoboats/item/SpeedUpgradeItem.java
@@ -1,8 +1,16 @@
 package com.landmaster.cargoboats.item;
 
 import com.landmaster.cargoboats.Config;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+
+import javax.annotation.Nonnull;
+import java.text.DecimalFormat;
+import java.util.List;
 
 public class SpeedUpgradeItem extends Item implements MotorboatUpgrade {
     public SpeedUpgradeItem(Properties properties) {
@@ -12,5 +20,16 @@ public class SpeedUpgradeItem extends Item implements MotorboatUpgrade {
     @Override
     public int maxUpgradeAmount(EntityType<?> entityType) {
         return Config.MOTORBOAT_SPEED_MULTIPLIERS.get().size();
+    }
+
+    @Override
+    public void appendHoverText(@Nonnull ItemStack stack, @Nonnull TooltipContext context, @Nonnull List<Component> tooltipComponents, @Nonnull TooltipFlag tooltipFlag) {
+        var multipliers = Config.MOTORBOAT_SPEED_MULTIPLIERS.get();
+        for (int i=0; i<multipliers.size(); ++i) {
+            tooltipComponents.add(Component.translatable(
+                    "tooltip.cargoboats.speed_upgrade", i+1,
+                    new DecimalFormat("0.0#").format(multipliers.get(i)))
+                    .withStyle(ChatFormatting.AQUA));
+        }
     }
 }
